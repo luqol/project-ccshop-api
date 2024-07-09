78a47fc7d4eb4fc526c198bfab97d5e7a26ce139
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-07-09 18:16:02 +0200

    Add hemlet

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use('/images', express.static('images'));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+  app.use(helmet());
   await app.enableShutdownHooks();
   await app.listen(8000);
 }
